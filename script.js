f8377e283f835c8510123b54ac76ad78f4a3c936
Dorazzi <henrique.reis2@estudante.ifms.edu.br>
2026-09-21 20:05:19 +0000

    feat: adiciona validacao do formulario

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ const botaoTema = document.getElementById("tema");
 botaoTema.addEventListener("click", function () {
     document.body.classList.toggle("escuro");
 });
+const formulario = document.querySelector("form");
+
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+
+    if (nome === "" || email === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+});
